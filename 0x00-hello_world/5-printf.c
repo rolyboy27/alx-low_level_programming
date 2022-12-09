2a26a838c5d1d648772449d6647c3d8995f8b731
roland ogbonna <electrorolando@gmail.com>
2022-12-08 19:09:11 -0800

    MY C COMMIT

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
 * main - prints out a string via printf
-* description: program that prints out the string s followed by a new line
+* Description: Program that prints out the string s followed by a new line
 *Return: int 0 when done and no errors
 */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
-
